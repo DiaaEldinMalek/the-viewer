31075aab9db51ee00b8f2e24e9f6e9e0ff5dfdce
DiaaEldinMalek <diaamalek3@gmail.com>
2025-03-26 03:05:07 +0200

    Scraper script

---
+++ _scrape_random_books.py
@@ -0,0 +1,19 @@
+import random
+from backend.gutenberg.controller import GutenController, GutenbergBookContent
+import pathlib
+import json
+
+
+ctrl = GutenController()
+for i in range(10):
+    idx = random.randint(1, 9999)
+    book_path = pathlib.Path("cache/" + str(idx))
+    book_path.mkdir(parents=True, exist_ok=True)
+
+    content = ctrl.fetch_book_content(idx)
+    metadata = ctrl.fetch_book_metadata(idx)
+
+    with open(book_path.joinpath("content.txt"), "w") as f:
+        f.write(content.content)
+    with open(book_path.joinpath("metadata.json"), "w") as f:
+        json.dump(metadata.model_dump(), f)
